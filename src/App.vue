a2903e6f24c317549af282d3af70147d6a75230e
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-02-10 18:49:14 +0200

    Add todolist & todoitems

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Todo Application</h1>
+    <!--v-bind для передачи данных в компоненты -->
+    <TodoList 
+      v-bind:todos="todos"
+    />
+  </div>
+</template>
+
+<script>
+//собака в названии пути указывает на папку src
+//что ооочень удобно
+import TodoList from '@/components/TodoList.vue';
+export default {
+  name: 'App',
+  //для обявсения переменных с данными
+  data() {
+    return {
+      todos: [
+        {id: 1, title: 'Buy the machinegun.', status: false},
+        {id: 2, title: 'Buy 1000 bullets.', status: true},
+        {id: 3, title: 'Buy the flower buket.', status: true}
+      ],
+    };
+  },
+  //для добавления и регистрации компонент
+  components: {
+    TodoList,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+        <!-- Для итерации по массивам нужно использоавать v-for -->
+        <!-- И в v-bind можно передавать нужный нам елемент в итерируемую компоненту -->
+      <TodoItem 
+        v-for="(todo, index) of todos" :key="index"
+        v-bind:todo="todo"
+        />
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue'
+
+export default {
+  //для того чтобы компонента знала что мы в нее передаем
+  //используется свойство props
+  //в масиве которого и прописываются названия переменных с нужными данными
+  props: ['todos'],
+  components: {
+    TodoItem,
+  },
+}
+</script>
